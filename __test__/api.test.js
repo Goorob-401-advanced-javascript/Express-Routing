52e858f433a8f47274f847a742f0b441981a956c
LTUC Student 1 <std1@QUDSLABS.LOCAL>
2020-02-01 23:56:19 +0200

    adding unpushed files

--- __test__/api.test.js
+++ __test__/api.test.js
@@ -2,15 +2,69 @@
 const {server}=require('../lib/server.js');
 const supergoose = require('@code-fellows/supergoose');
 const mockRequest = supergoose(server);
-describe('Food API' ,()=>{
-    let testObj = { name : 'apple' , calories :25 , type:'FRUIT'};
-    return mockRequest.post('/api/v1/food')
-    .send(testObj)
-    .then(data =>{
-        console.log('******' , data.body);
-        let record = data.body  ;
-        Object.keys(testObj).forEach(key =>{
-            expect(reccord[key]).toEquel(testObj[key]);
+describe('Categories API' ,()=>{
+  it('get a categorie item', () => {
+
+    return mockRequest.get('/api/v1/categories')
+      .then(res => {
+        expect(res.status).toEqual(200)
+      })
+  });
+  it ('get ()' ,() =>{
+    let newCategory = {name:'shoes' , type: 'clothes'};
+    return mockRequest.get('/api/v1/categories')
+      .send(newCategory)
+      .then(data=>{
+        console.log(data.body._id);
+        return mockRequest.get(`api/categories/${data.body}`)
+          .then(data =>{
+            expect(data.status).toBe(200);
+            expect(data.body[0].name).toEqual('shoes');
+          })
+      })
+  });
+  it('post () ' , ()=> {
+    let newCategory = {name:'shoes' , type: 'clothes'};
+    return mockRequest.post('/api/v1/categories')
+      .send(newCategory)
+      .then(data => {
+        Object.keys(newCategory).forEach(val => {
+          expect(data.body[val]).toEqual(newCategory[val]);
         });
-    })
+      });
+  });
+
 });
+describe('Products API' ,()=>{
+  it('get a product item', () => {
+
+    return mockRequest.get('/api/v1/categories')
+      .then(res => {
+        expect(res.status).toEqual(200)
+      })
+  });
+  it ('get ()' ,() =>{
+    let newProduct = {name:'nike' , price: 78};
+    return mockRequest.get('/api/v1/categories')
+      .send(newProduct)
+      .then(data=>{
+        console.log(data.body._id);
+        return mockRequest.get(`api/Products/${data.body}`)
+          .then(data =>{
+            expect(data.status).toBe(200);
+            expect(data.body[0].price).toEqual(78);
+          })
+      })
+  });
+  it('post () ' , ()=> {
+    let newProduct= {name:'shoes' , type: 'clothes'};
+    return mockRequest.post('/api/v1/categories')
+      .send(newProduct)
+      .then(data => {
+        Object.keys(newProduct).forEach(val => {
+          expect(data.body[val]).toEqual(newProduct[val]);
+        });
+      });
+  });
+
+}) ;
